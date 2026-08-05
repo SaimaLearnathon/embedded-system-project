e027a9df5c239da1ed73ba3761459176258090c5
SaimaLearnathon <saimafarhanaislam@gmail.com>
2026-08-05 22:18:25 +0600

    blinking LED done successfully

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,18 +1,41 @@
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
+#define LED_PORT  GPIOC
+#define LED_PIN   GPIO13
 
-static void rcc_setup(void){
-
-rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
-
-
+static void rcc_setup(void)
+{
+	/* BluePill has an 8 MHz crystal on the board (HSE). Run at 72 MHz. */
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 }
 
-int main (void){
+static void gpio_setup(void)
+{
+	/* Enable the peripheral clock for port C (where PC13 LED lives). */
+	rcc_periph_clock_enable(RCC_GPIOC);
 
-    while(1){
+	/* PC13 LED -> push-pull output, 2 MHz. */
+	gpio_set_mode(LED_PORT, GPIO_MODE_OUTPUT_2_MHZ,
+	              GPIO_CNF_OUTPUT_PUSHPULL, LED_PIN);
+}
 
-    }
-    return 0;
+int main(void)
+{
+	rcc_setup();
+	gpio_setup();
 
+	while (1) {
+		gpio_clear(LED_PORT, LED_PIN);  /* LED ON (active-low) */
+		for (volatile uint32_t i = 0; i < 5000000; i++) {
+			__asm__("nop");
+		}
+
+		gpio_set(LED_PORT, LED_PIN);    /* LED OFF */
+		for (volatile uint32_t i = 0; i < 5000000; i++) {
+			__asm__("nop");
+		}
+	}
+
+	return 0;
 }
---
+++ app/src/firmware.d
@@ -0,0 +1,19 @@
+src/firmware.o: src/firmware.c \
+ ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ D:/lib/gcc/arm-none-eabi/15.3.1/include/stdint.h \
+ D:/arm-none-eabi/include/stdint.h \
+ D:/arm-none-eabi/include/machine/_default_types.h \
+ D:/arm-none-eabi/include/sys/features.h \
+ D:/arm-none-eabi/include/_newlib_version.h \
+ D:/arm-none-eabi/include/sys/_intsup.h \
+ D:/arm-none-eabi/include/sys/_stdint.h \
+ D:/lib/gcc/arm-none-eabi/15.3.1/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
